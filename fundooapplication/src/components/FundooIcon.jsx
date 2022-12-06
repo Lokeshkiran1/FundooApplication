eebc9e19a6b9d33b684ac1b0147b778b53e1d7cf
LokeshKiran <lokeshkskiran@gmail.com>
2022-12-06 08:37:36 +0530

    [Lokesh]Added:Sign in front end feature for Fundoo application

---
+++ fundooapplication/src/components/FundooIcon.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class FundooIcon extends Component {
+    render() {
+        return (
+            <div className="fitWidth">
+                <span style={{ color: '#4385F4', fontSize: '30px', fontWeight: 400 }}>F</span>
+                <span style={{ color: '#EA4335', fontSize: '30px', fontWeight: 400 }}>u</span>
+                <span style={{ color: '#FBBD35', fontSize: '30px', fontWeight: 400 }}>n</span>
+                <span style={{ color: '#4385F4', fontSize: '30px', fontWeight: 400 }}>d</span>
+                <span style={{ color: '#4EA853', fontSize: '30px', fontWeight: 400 }}>o</span>
+                <span style={{ color: '#EA4335', fontSize: '30px', fontWeight: 400 }}>o</span>
+            </div>
+        )
+    }
+}
+export default FundooIcon;
---
+++ fundooapplication/src/signin/Signin.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Signin.css";
+import FundooIcon from"../components/FundooIcon";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button'
+const SignIn=()=>{
+    return(
+        <div className="loginPage">
+            <div className="loginCard">
+                <div className="columncenter">
+                    <FundooIcon className="fitWidth" />
+                    <h3 className="headText">Sign in</h3>
+                    <span className="subText">Use your Google Account</span>
+                </div>
+                <div className="columnform" >
+                    <TextField id="outlined" label="Email" variant="outlined" margin="normal" />
+                    <TextField id="outlined" label="Password" variant="outlined" margin="normal" />
+                </div>
+                <div className="notComputer">Not your computer? Use Guest mode to sign in privately.
+                    <a className="link" href="https://support.google.com/chrome/answer/6130773?hl=en">Learn More</a>
+                </div>
+                <div className="buttonRegister" >
+                       <div className="row" style={{ justifyContent: 'space-between' }}>
+                            <div className="create">Create Account</div>
+                            <Button className="loginButton" variant="contained">Login</Button>
+                       </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+export default SignIn;
